9d769feed5b60613b2c294b30bc886d65b05063a
Imran <imran.kazi@kcsitglobal.com>
2023-05-02 12:23:04 +0530

    cicd with build and publish-14

---
+++ mypackage/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getStringLength = void 0;
+const getStringLength = (str) => {
+    return str.length;
+};
+exports.getStringLength = getStringLength;
+//# sourceMappingURL=index.js.map
